f4af6972505c3f3fa9fd4ec318a57525c53464d1
persadenis <denis.persa@stud.ubbcluj.ro>
2026-08-22 12:21:45 +0300

    Migrate application to Spring Boot

---
+++ src/main/java/com/cabanateo/CabanaTeoApplication.java
@@ -0,0 +1,13 @@
+package com.cabanateo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabanaTeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabanaTeoApplication.class, args);
+	}
+
+}
